638e33857f0cedd5f9f16e6f517eb98a71405439
Brian Dyko <briandyko@gmail.com>
2019-05-12 10:37:04 -0400

    utils test

--- spec/demo_with_typescript_spec.ts
+++ spec/demo_with_typescript_spec.ts
@@ -1,4 +1,4 @@
-import { add } from "./utils";
+import { add, percentage } from "./utils";
 
 describe('writing specs in typescript', () => {
     it('is easy', () => {
@@ -6,5 +6,13 @@ describe('writing specs in typescript', () => {
     });
     it('can add DELETE ME', () => {
         expect(add(2,2)).toBe(4);
+    });
+});
+
+describe('does what tips supposed to do', () =>{
+    it('calculates percentages', () => {
+        let base: number = 100;
+        let tipAmount: number = 20;
+        expect(percentage(100,20)).toBe(5);
     });
 });
