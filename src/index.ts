b9c8af2a39cd1d093dcbc1d7531ded627a6b2de8
Brian Dyko <briandyko@gmail.com>
2019-05-16 13:25:11 -0400

    data-amount html

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { billTotal, formatPercentage, formatterUSD, tipCalculation } from '../sp
 import './styles.css';
 console.log('Ready to Party');
 
+let tipstore: number;
 let bill = document.getElementById("okay");
 bill.addEventListener('input', displayBill);
 bill.addEventListener('input', startMath)
@@ -14,21 +15,26 @@ function tipTime(e: any) {
   console.log(selection);
   let tipString = 't' + selection;
   console.log(tipString);
+  let theAmountToTip = +this.dataset.amount;
+  console.log(theAmountToTip);
   if (tipString === 't10%') {
     let tippy: any = document.getElementById('t10');
     tippy.disabled = true;
+    tipstore = 10;
     document.getElementById("tipPercentage").innerHTML = `Amount of tip: ${(selection)}`;
     document.getElementById('tipInfo').innerHTML = `You are tipping: ${(selection)}`
   }
   if (tipString === 't15%') {
     let tippy: any = document.getElementById('t15');
     tippy.disabled = true;
+    tipstore = 15;
     document.getElementById("tipPercentage").innerHTML = `Amount of tip: ${(selection)}`;
     document.getElementById('tipInfo').innerHTML = `You are tipping: ${(selection)}`
   }
   if (tipString === 't20%') {
     let tippy: any = document.getElementById('t20');
     tippy.disabled = true;
+    tipstore = 20;
     document.getElementById("tipPerecentage").innerHTML = `Amount of tip: ${(selection)}`;
     document.getElementById('tipInfo').innerHTML = `You are tipping: ${(selection)}`
   }
@@ -45,7 +51,6 @@ function displayBill(num: any) {
 
 function startMath(math: any) {
   let now = parseFloat(math.srcElement.value);
-  let tipPer = 10
   let x = tipCalculation(now, 10);
   document.getElementById("tipAmount").innerHTML = `Amount of tip: ${formatterUSD.format(x)}`;
   let z = billTotal(now, x);
@@ -53,4 +58,4 @@ function startMath(math: any) {
 }
 
 
-
+//input-type numbers on the input thing...but will still allow negatives
