dec09790869c2c8c8da9adaa8cb07f544f207659
Brian Dyko <briandyko@gmail.com>
2019-05-15 23:20:07 -0400

    added basic tipping functionality

--- spec/utils.ts
+++ spec/utils.ts
@@ -1,14 +1,15 @@
-export const add = (a:number, b:number) => a + b;
-export function tipCalculation(billAmount:number, tipSelection:number){
-    const percentify = (tipAmount:number) => tipAmount/100;
+export const add = (a:number, b:any) => a + b;
+export function tipCalculation(billAmount:any, tipSelection:any){
+    const percentify = (tipAmount:any) => tipAmount/100;
     return (billAmount * percentify(tipSelection));
 } 
-export function billTotal(billAmount:number, tipAmount:number){
+export function billTotal(billAmount:any, tipAmount:any){
     return add(billAmount, tipAmount);
 }
 export const formatterUSD = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
   });
-  export const formatPercentage  = (percentage:number) => percentage + '%';
+  export const formatPercentage  = (percentage:any) => percentage + '%';
+
   
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-import { billTotal, formatPercentage, formatterUSD, tipCalculation } from "../spec/utils";
+import { billTotal, formatPercentage, formatterUSD, tipCalculation } from '../spec/utils';
 import './styles.css';
 console.log('Ready to Party');
 
-const billAmount:number = 125; //input from User on form
-console.log(formatterUSD.format(billAmount));
+let bill = document.getElementById("okay");
+bill.addEventListener('input', displayBill);
+bill.addEventListener('input', startMath)
 
-const tipPercentage:number = 10;  //id of selected button
+
+const tipPercentage:any = 10;  //id of selected button
 console.log(formatPercentage(tipPercentage));
 
-const tipAmount:number = tipCalculation(billAmount, tipPercentage) //amount of tip
-console.log(formatterUSD.format(tipAmount));
 
-const grandTotalBill:number = billTotal(billAmount, tipAmount);
-console.log(formatterUSD.format(grandTotalBill));
+function displayBill(num:any){
+  let what:number = num.srcElement.value;
+  document.getElementById("ba").innerHTML = `Bill Amount: ${formatterUSD.format(what)}`; 
+}
+
+function startMath(math:any){
+let now = parseFloat(math.srcElement.value);
+let tipPer = 10
+let x = tipCalculation(now,10);
+document.getElementById("tipAmount").innerHTML = `Amount of tip: ${formatterUSD.format(x)}`; 
+let z = billTotal(now, x);
+document.getElementById("billTotal").innerHTML = `Total to be Paid: ${formatterUSD.format(z)}`;
+}
+
+
+
